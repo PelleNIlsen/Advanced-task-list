25c60c158adc4ab9c8f71f62439ddf4d2354c21e
PelleNIlsen <pelle@f-w.no>
2022-10-27 09:26:45 +0200

    Changelog: - Can now delete Lists - Can now delete Tasks - Can now delete Subtasks - Can now add custom Notes

--- Advanced Task List/script.js
+++ Advanced Task List/script.js
@@ -52,6 +52,12 @@ function createList($name) {
     let $h3 = $("<h3>" + $name + "</h3>");
         $h3.attr("class", "listTitle")
 
+    let $deleteList = $("<button>Delete list</button>");
+        $deleteList.attr("class", "deleteList");
+        $deleteList.attr("onclick", "deleteList(event)");
+    
+    $h3.append($deleteList);
+
     let $form = $("<form>");
         $form.attr("class", "createTask");
         $form.attr("id", "taskForm");
@@ -112,7 +118,14 @@ function createTask() {
     let $taskDiv = $("<div>");
         $taskDiv.attr("class", "task");
     // Create a new h3 with class "taskTitle" and the value of $taskName
- 
+    let $addSubtask = $("<button>Add subtask</button>");
+        $addSubtask.attr("class", "addSubtask");
+        $addSubtask.attr("onClick", "addSubtask(event)");
+    let $deleteTask = $("<button>Delete task</button>");
+        $deleteTask.attr("class", "deleteTask");
+        $deleteTask.attr("onClick", "deleteTask(event)");
+
+    
     
     // Create a new button with class "addNote" which will add a <h5> with class "note" and text "New Note" and place it underneath the <h4> with class "taskTitle"
     let $addNoteButton = $("<button>+</button>");
@@ -147,13 +160,130 @@ function createTask() {
     let $taskListDiv = $(event.target).parent().parent().children()[2];
     // If the class name is "taskList", append the div to the parent parents third child
     // Append $taskDiv to $taskListDiv
+    $taskDiv.append($addSubtask);
+    $taskDiv.append($deleteTask);
     $($taskListDiv).append($taskDiv);
 }
 
 function addNote() {
-    // Create a new h5 with class "note" and text "New Note"
-    let $note = $("<h5>New note</h5>");
-        $note.attr("class", "note");
-    // Append the h5 to the parent div with class "task"
-    $(event.target).parent().append($note);
+    // Create an input with type "text" and class "noteInput"
+    let $noteInput = $("<input>");
+        $noteInput.attr("type", "text");
+        $noteInput.attr("class", "noteInput");
+        $noteInput.attr("placeholder", "New Note...");
+    // Create a button with class "noteSubmit" and text "Submit"
+    let $noteSubmit = $("<button>Submit</button>");
+        $noteSubmit.attr("class", "noteSubmit");
+        $noteSubmit.attr("onClick", "submitNote()");
+    // Append the input and button to the parent div with class "task"
+    $(event.target).parent().append($noteInput);
+    $(event.target).parent().append($noteSubmit);
+}
+
+function submitNote() {
+    // Get the value of the input with class "noteInput"
+    let $noteInput = $(event.target).parent().children()[1].value;
+    // If $noteInput is more than 50 characters add "\n" inside the string every 50 characters
+    let $array = [];
+    if ($noteInput.length > 50) {
+        for (let i = 0; i < $noteInput.length; i = i + 50) {
+            $array.push($noteInput.slice(i, i + 50));
+        }
+        console.log($array);
+    }
+    // Create a new <h5> with class "note" and the value of $noteInput
+    let $noteH5;
+    if ($noteInput.length < 50) {
+        $noteH5 = $("<h5>" + $noteInput + "</h5>");
+        $noteH5.attr("class", "note");
+        // Append the <h5> to the parent div with class "task"
+        $(event.target).parent().append($noteH5);
+    } else {
+        for (let i = 0; i < $array.length; i++) {
+            $noteH5 = $("<h5>");
+            $noteH5.attr("class", "note");
+            $noteH5.text($array[i]);
+            $(event.target).parent().append($noteH5);
+        }
+    }
+    // Remove the input and button from the parent div with class "task"
+    $(event.target).parent().children()[1].remove();
+    $(event.target).parent().children()[1].remove();
+
+}
+
+function deleteList(event) {
+
+    // Ask user if they are sure they want to delete the list
+    let $sure = confirm("Are you sure you want to delete this list?");
+    // If they are sure, delete the list
+    if ($sure) {
+        let $list = $(event.target).parent().parent();
+        // Remove the div from the DOM
+        $list.remove();
+    } else {
+        return;
+    }
+}
+
+function deleteTask(event) {
+    // Ask user if they are sure they want to delete the task
+    let $sure = confirm("Are you sure you want to delete this task?");
+    // If they are sure, delete the task
+    if ($sure) {
+        let $task = $(event.target).parent();
+        // Remove the div from the DOM
+        $task.remove();
+    } else {
+        return;
+    }
+}
+
+function addSubtask(event) {
+    let $subtask = $("<div>");
+        $subtask.attr("class", "subtask");
+    let $subtaskInput = $("<input>");
+        $subtaskInput.attr("type", "text");
+        $subtaskInput.attr("placeholder", "New Subtask Name...");
+    let $subtaskButton = $("<button>Create New Subtask</button>");
+        $subtaskButton.attr("type", "button");
+        $subtaskButton.attr("onClick", "createSubtask(event)");
+    $subtask.append($subtaskInput);
+    $subtask.append($subtaskButton);
+    $(event.target).parent().append($subtask);
+}
+
+function createSubtask(event) {
+    let $subtaskName = $(event.target).parent().children()[0].value;
+    if ($subtaskName[0] == " " || $subtaskName == "") {
+        $(".error").text("Invalid name!");
+        $(".error").css("color", "red");
+        $(".error").css("visibility", "visible");
+        setTimeout( function() {
+            $(".error").css("visibility", "hidden");
+        }, 5000);
+        $(event.target).parent().children()[0].value = ""; 
+        return;
+    }
+    let $subtask = $("<div>");
+        $subtask.attr("class", "subtask");
+    let $subtaskH4 = $("<h4>" + $subtaskName + "</h4>");
+    let $deleteSubtask = $("<button>Delete subtask</button>");
+        $deleteSubtask.attr("class", "deleteSubtask");
+        $deleteSubtask.attr("onClick", "deleteSubtask(event)");
+    let $addNoteButton = $("<button>+</button>");
+        $addNoteButton.attr("class", "addNote");
+        $addNoteButton.attr("onClick", "addNote()");
+        // Add flaoting right to the button
+        $addNoteButton.css("float", "right");
+    let $addSubtask = $("<button>Add subtask</button>");
+        $addSubtask.attr("class", "addSubtask");
+        $addSubtask.attr("onClick", "addSubtask(event)");
+    
+    $subtaskH4.append($addNoteButton);
+    $subtask.append($subtaskH4);
+    $subtask.append($addSubtask);
+    $subtask.append($deleteSubtask);
+    $(event.target).parent().parent().append($subtask);
+    $(event.target).parent().remove();
 }
